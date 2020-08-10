7a650d464f3c59ab6d4e7bc80338d7f513278d77
i4j5 <i4j5@ya.ru>
2020-08-10 17:50:34 +0300

    -

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect()->route('home');
+    // return redirect()->route('home');
+    return view('welcome');
 });
 
 Auth::routes();
